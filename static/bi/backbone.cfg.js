eb12c705891101e2e3319277eef9fb7bf73399a9
shuqizhao <shuqizhao@beisen.com>
2016-10-09 17:26:06 +0800

    fix table legnth bug

--- static/bi/backbone.cfg.js
+++ static/bi/backbone.cfg.js
@@ -181,16 +181,9 @@
         }
     };
     $.fn.dataTableExt.oApi.fnStandingRedraw = function(oSettings, isFirstLoad, oTable) {
-        //redraw to account for filtering and sorting
-        // concept here is that (for client side) there is a row got inserted at the end (for an add)
-        // or when a record was modified it could be in the middle of the table
-        // that is probably not supposed to be there - due to filtering / sorting
-        // so we need to re process filtering and sorting
-        // BUT - if it is server side - then this should be handled by the server - so skip this step
         if (oSettings.oFeatures.bServerSide === false) {
             var before = oSettings._iDisplayStart;
             oSettings.oApi._fnReDraw(oSettings);
-            //iDisplayStart has been reset to zero - so lets change it back
             oSettings._iDisplayStart = before;
             oSettings.oApi._fnCalculateEnd(oSettings);
         }
@@ -208,6 +201,8 @@
                 oSettings._iDisplayStart = _iDisplayStart
                 oSettings._iDisplayLength = _iDisplayLength
                 oSettings.oPreviousSearch.sSearch = sSearch
+                select = $('.searchDataTableBody').find('select');
+                select.find("option[value='" + _iDisplayLength + "']").attr("selected", "selected");
             } else {
                 return
             }
