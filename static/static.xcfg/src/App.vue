57fca212eba5af79cf44e5c3dff4c09c3fb7828f
shuqizhao <shuqizhao@qq.com>
2018-04-17 14:02:43 +0800

    new version

--- static/static.xcfg/src/App.vue
+++ static/static.xcfg/src/App.vue
@@ -1,14 +1,18 @@
 <template>
- <mimiApp></mimiApp>
+<div>
+    <mimiApp></mimiApp>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "app",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
     };
   }
 };
 </script>
+
+<style>
+
+</style>
--- static/static.xcfg/webpack.config.js
+++ static/static.xcfg/webpack.config.js
@@ -33,10 +33,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: ['es2015']
-                 }
+                exclude: /node_modules/
             },
             {
                 test: /\.(png|jpg|gif|svg)$/,
